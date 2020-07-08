32b1b8c777c85646dfd5638e5d29cbea16a2a45b
Ramim <ramimhridoy512@gmail.com>
2020-07-08 23:36:20 +0600

    first commit

---
+++ app/src/main/java/com/example/backalart/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.backalart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onBackpressed(){
+
+
+    }
+
+
+}
